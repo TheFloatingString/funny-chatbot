aa3dadef328e77e74074aa2bdc0b690f4dd64319
TheFloatingString <laurencel2001@gmail.com>
2017-08-12 21:45:47 -0400

    Chapter 1-1.4
    Word count

--- NLTK3_Book/Chapter_01/Code_Chapter_01.py
+++ NLTK3_Book/Chapter_01/Code_Chapter_01.py
@@ -1,6 +1,10 @@
 """ 
-This code examplifies Chapter 1 of the NLTK Book,
+This code exemplifies Chapter 1 of the NLTK Book,
 which can be read at: http://www.nltk.org/book/ 
 """ 
 
-import NLTK 
+import nltk 
+from nltk.book import *
+
+# 1.4 Counting vocabulary
+print("Number of words in text 3: {}".format(len(text3)))
